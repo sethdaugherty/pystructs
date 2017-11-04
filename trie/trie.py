fc4a259c4e19ecd02fa3417780fd8a9437bf0042
Seth Daugherty <daugherty.seth@gmail.com>
2017-11-04 16:15:47 +0000

    Started working on the trie implementation

---
+++ trie/trie.py
@@ -0,0 +1,18 @@
+
+
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+
+
+class Trie:
+
+    def __init__(self):
+        self.head = Node()
+
+
+    def addWord(self, word):
+
+
+
